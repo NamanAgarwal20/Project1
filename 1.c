c9b62d79aa376aa9c7cb786ff0bf800c65f4569d
NamanAgarwal20 <141299696+NamanAgarwal20@users.noreply.github.com>
2023-08-03 11:42:08 +0530

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+ int a,b,c;
+ printf("Enter your birth year:");
+ scanf("%d",&a);
+ printf("Enter the current year:");
+ scanf("%d",&b);
+ printf("Adding both the years:");
+ c=a+b;
+ printf("%d",c);
+}
